094992a210765382934efd079106aa50ee102109
manibalaji003 <89721684+manibalaji003@users.noreply.github.com>
2023-11-29 17:59:59 +0530

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,9 +1,23 @@
 import string
 import random
 
-def generate_password(length):
-    all_chars = string.ascii_letters + string.digits + string.punctuation
-    password = ''.join(random.choice(all_chars) for i in range(length))
+def generate_strong_password(length):
+    if length < 4:
+        print("Password length should be at least 4")
+        return
+
+    all_chars = [random.choice(string.ascii_lowercase),
+                 random.choice(string.ascii_uppercase),
+                 random.choice(string.digits),
+                 random.choice(string.punctuation)]
+
+    if length > 4:
+        all_chars += [random.choice(string.ascii_letters + string.digits + string.punctuation)
+                      for _ in range(length-4)]
+
+    random.shuffle(all_chars)
+    password = ''.join(all_chars)
     return password
-a=int(input("Enter the length of password: "))
-print(generate_password(a))
+
+a = int(input("Enter the length of password: "))
+print(generate_strong_password(a))
